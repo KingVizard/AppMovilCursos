74a880949d212751ce5e2fc4f739a53e02d98f54
pablo <pablopgzz14@gmail.com>
2023-10-20 16:26:41 -0600

    Sin Cambios

--- AppMovilCursos/Views/Inicio.xaml.cs
+++ AppMovilCursos/Views/Inicio.xaml.cs
@@ -10,7 +10,7 @@ using Xamarin.Forms.Xaml;
 namespace AppMovilCursos.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class Inicio : ContentPage
+    public partial class Inicio : TabbedPage
     {
         public Inicio()
         {
